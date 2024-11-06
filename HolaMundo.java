c4619cf904ce4edbfa91f311f4118cee80fd8ada
Antonio-Valverde <avallem860@iesalmudeyne.es>
2024-11-06 09:33:45 +0100

    Subidad de otra rama con cosas diferemtes

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Hola, Vamos a modificar para generarCOnflicto");
+        System.out.println("Hola, Vamos a modificar cosas");
     }
 }
